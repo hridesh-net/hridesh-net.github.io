60977b47789c490f8e0cb968965ad0085f6e1a94
Hridesh sharma <61771411+hridesh-net@users.noreply.github.com>
2021-11-13 11:15:06 +0530

    skills js bug removed

--- js/main.js
+++ js/main.js
@@ -29,14 +29,14 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // ############ ACCORDION SKILLS ##############
-const skillsCon = document.getElementsByClassName('skills_content');
-const skillsHeader = document.querySelectorAll('.skills_header');
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header')
 
-function togskills() {
-    let itemClass = this.parentNode.ClassName
+function toggleSkills() {
+    let itemClass = this.parentNode.className
 
-    for (i = 0; i < skillsCon.length; i++) {
-        skillsCon[i].className = 'skills_content skills_close'
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills_content skills_close'
     }
     if (itemClass === 'skills_content skills_close') {
         this.parentNode.className = 'skills_content skills_open'
@@ -44,5 +44,5 @@ function togskills() {
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', togskills)
+    el.addEventListener('click', toggleSkills)
 })
